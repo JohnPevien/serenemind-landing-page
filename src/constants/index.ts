2ea77452e1a99a26b48080ea2a87a728cac697fb
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-05-26 23:30:43 +0800

    add constants for data that will be used in the page

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export * from "./navLinks";
+export * from "./features";
+export * from "./feedback";
+export * from "./stats";
+export * from "./footerLinks";
+export * from "./socialMedia";
+export * from "./clients";
